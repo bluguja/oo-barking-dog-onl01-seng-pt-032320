31b1e2d4c38eed96a49a102cce732c755c3f4755
Bala Luguja <bluguja@gmail.com>
2020-04-10 03:01:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,20 @@
-class Dog# Your code goes here!
-end
 
-fido = Dog.new
-fido.name = "Fido"
- 
-fido.name
-# => "Fido"
- 
-fido.bark
-#woof!
+# Your code goes here!
+
+class Dog
+  def name=(dogs_name)
+    @name = dogs_name
+  end 
+
+  def name
+    @name
+  end 
+  
+end 
+
+#class Dog
+  def bark
+    puts "woof!"
+  end 
+  
+end 
